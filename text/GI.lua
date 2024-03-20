9530d01a93d74f4971016901a60eebda5a5b743d
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-20 15:13:58 +0500

    Create GI.lua

---
+++ text/GI.lua
@@ -0,0 +1,13 @@
+-- GI = GameInit
+GameInit = {}
+function GameInit:init()
+local function CustomDialog()
+ CustomDialog:create()
+  CustomDialog:setTitleText("Hack Info")
+  CustomDialog:setDescText("This is written by KyleMV, version 1.5.0, Latest = true \n ^FFFF0000Developers: \n KyleMV ( HEad DEV ) \n Mr Pop Cat ( Gave Bypass files ) \n Floppa ( For BEing Lazy and good dev )
+    CustomDialog:button1("OK")
+      CustomDialog:button2("Close")
+function button2()
+        exit.dialog()
+      end)
+  end(...)
