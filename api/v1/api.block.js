ca8345ae9669745c816f03a1793e996f371a3e89
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-23 19:02:47 +0500

    Create api.block.js

---
+++ api/v1/api.block.js
@@ -0,0 +1,6 @@
+apiNAME="com.blockymods.api"
+min_engine_version=[2,74,2] // Dont worry, itll still work even tho if its lower versions. min engine version checks the version of the app and if it is lower than 2.74.2 then it will crash :)
+modules = [-83, 843, -437]
+function apiv1()
+console.log("Api is on")
+apiv1()
