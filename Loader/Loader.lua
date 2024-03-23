a03d40bb49d5c8f5e26cc7f720bda50d951990da
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-23 22:15:38 +0500

    Create Loader.lua

---
+++ Loader/Loader.lua
@@ -0,0 +1,3 @@
+local Hack = require(FilePath1)
+-- local FilePath1 = "sdcard/D_OP_DIR/HackLoaderENC.lua"
+return(function(local e="vgEDXfc=="(LLLNENDGEBDEYGBeDeBDebGDYEGDBYEDGBBDEBebdGYbedgbgydbgYEeBY==)))local JU = {"...COOKIE...ND..POOKIE..OBFUSCATOR.."{"#$#$YDB$GU$BFG%%D%%D%$%RC%RFR#E#%E#D%FRR%FFEC%RRC%%EDEFF%RECD%RFCDE%RFEDC%rbfnCR%ECD5rfbecdf5bredF5brEDFfbef%MK+mKF=mftff%MkT5m5mffm5tm%TkfokTft=fF++M%TFm5tmkftmFTmfktf+M%T%Tmtmm5okmok5omk5tom5omMK+++&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",k"segdegd"}},autodec=table[2387478(-324776345679347r674-236)]
