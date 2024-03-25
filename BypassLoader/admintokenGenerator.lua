21ed51c58547b6b0039f8d07cfd21cf92f87cfcb
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-25 18:12:27 +0500

    Create admintokenGenerator.lua

---
+++ BypassLoader/admintokenGenerator.lua
@@ -0,0 +1,2 @@
+local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+return(function(...)local h={REDfEDCTRDFTWEGDWTFWREDhyxsWTDCWWXTFWCFWRDSFSxbegFFWXFRWFRbdbgEFCcjiJMECmRFBCBGZHWSNNCgt}local function unpack()table.unpack;setmetatable;unpack()end(...)
