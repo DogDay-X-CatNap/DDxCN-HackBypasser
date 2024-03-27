f7e974218cb37f5e14b9a55a2a22ee24d4b3bddf
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-27 14:02:36 +0500

    Create gamefiles.lua

---
+++ loading_background/gamefiles.lua
@@ -0,0 +1,15 @@
+----------------------------------------------------
+--[[
+This file was generated with CatNap Obfuscator v6
+]]--
+-----------------------------------------------------
+local SETGB = "4gb RAM"
+GMHelper:nnehbce()
+s = {}
+if s.GB == 4 then print("GO ON")
+  GMHelper:hbcvvtgrcvrccsexhsgse()
+  if s.GameGG == on then print("EXIT CODE = GGDETECTED")
+  end;end;
+GMHelper:isoinstaller()
+if s.GGOverlay == on then  GMHelper:hbcvvtgrcvrccsexhsgse();end
+gmLEDEYDGGE()
